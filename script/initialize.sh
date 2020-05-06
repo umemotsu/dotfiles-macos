2e81e156ed249f5a726daae1c581c8908247a95c
Kazutoshi Umemoto <umemoto@tkl.iis.u-tokyo.ac.jp>
2020-05-06 16:50:00 +0900

    feat: Add script file for running app-specific initializations

---
+++ script/initialize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOT_ROOT="$(cd $(dirname $0)/.. && pwd)"
+source "${DOT_ROOT}/script/utils.sh"
+
+echo
+echo_info "Initializing applications..."
+for init_file in $(find "${DOT_ROOT}/etc" -type f | sort); do
+    if [ ! -x "${init_file}" ]; then
+        echo_warning "Ignore non-executable file (${init_file})."
+        continue
+    fi
+    "${init_file}"
+done
