164418505990625dbd3bc86d2a768f574477b23c
Kazutoshi Umemoto <umemoto@tkl.iis.u-tokyo.ac.jp>
2020-05-06 18:09:18 +0900

    feat: Add initialization for mackup

---
+++ etc/mackup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+DOT_ROOT="$(cd $(dirname $0)/.. && pwd)"
+source "${DOT_ROOT}/script/utils.sh"
+
+echo
+echo_info "mackup"
+echo_info "Restoring application settings synced by mackup..."
+mackup --verbose restore
