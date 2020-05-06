2feba3500eb3bb79c93ca2f2f31cee187305fb71
Kazutoshi Umemoto <umemoto@tkl.iis.u-tokyo.ac.jp>
2020-05-06 20:44:24 +0900

    fix: Read only shell scripts (with .sh extension) during initialization

--- script/initialize.sh
+++ script/initialize.sh
@@ -7,7 +7,7 @@ source "${DOT_ROOT}/script/utils.sh"
 
 echo
 echo_info "Initializing applications..."
-for init_file in $(find "${DOT_ROOT}/etc" -type f | sort); do
+for init_file in $(find "${DOT_ROOT}/etc" -type f -name "*.sh" | sort); do
     if [ ! -x "${init_file}" ]; then
         echo_warning "Ignore non-executable file (${init_file})."
         continue
