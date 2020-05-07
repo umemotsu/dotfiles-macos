1fed9f025a7953428637a30df386d22f054ef2c2
Kazutoshi Umemoto <umemoto@tkl.iis.u-tokyo.ac.jp>
2020-05-07 15:22:12 +0900

    feat: Add script file for installation (both deployment and initialization)

---
+++ script/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+DOT_ROOT="$(cd $(dirname $0)/.. && pwd)"
+source "${DOT_ROOT}/script/utils.sh"
+
+echo
+echo_info "----------"
+echo_info "DEPLOYMENT"
+echo_info "----------"
+"${DOT_ROOT}/script/deploy.sh"
+
+echo
+echo_info "--------------"
+echo_info "INITIALIZATION"
+echo_info "--------------"
+"${DOT_ROOT}/script/initialize.sh"
